3fed235c6cf24e180b6d516bdf060de5e335cfe2
mohan rijal <mohanrijal@mohans-MacBook-Air.local>
2025-12-28 17:13:06 +0530

    vercel-config

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -68,7 +68,7 @@ const Dashboard = () => {
 
                 <NavLink className={({isActive})=> `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-100 border-r-4 border-blue-500 '}`}  to={'/dashboard/view-applications'}>
                       <img className='min-w-4' src={assets.person_tick_icon} alt="" /> 
-                      <p className='max-sm:hidden'>View job</p>
+                      <p className='max-sm:hidden '>View job</p>
                 </NavLink>
             </ul>
         </div>
